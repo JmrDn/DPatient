79bf0366b04b721f7e18420f10c502edc1aa7666
Joma <ptc.jomaridanao@gmail.com>
2024-01-13 17:45:40 +0800

    Nearly final

--- app/src/main/java/com/example/dpatient/Adapter/ConnectedWithAdapter.java
+++ app/src/main/java/com/example/dpatient/Adapter/ConnectedWithAdapter.java
@@ -1,17 +1,23 @@
 package com.example.dpatient.Adapter;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dpatient.Homepage1;
 import com.example.dpatient.R;
 import com.example.dpatient.UserDetails;
 import com.example.dpatient.model.ConnectWithModel;
@@ -51,6 +57,8 @@ public class ConnectedWithAdapter extends RecyclerView.Adapter<ConnectedWithAdap
         holder.userId = model.getUserId();
         holder.profilePic = model.getProfilePicture();
         holder.dateAndTime.setText(model.getDateAndTime());
+        String userIdString = model.getUserId();
+        String doctorName = model.getName();
 
         if (holder.profilePic != null){
             StorageReference storageReference = FirebaseStorage.getInstance().getReference("images/"+ holder.profilePic);
@@ -79,10 +87,82 @@ public class ConnectedWithAdapter extends RecyclerView.Adapter<ConnectedWithAdap
 
         }
 
+        holder.disconnectBtn.setOnClickListener(v->{
+            showDisconnectDialog(userIdString, doctorName);
+        });
 
 
 
 
+
+
+
+
+    }
+
+    private void showDisconnectDialog(String userIdString, String doctorName) {
+
+        Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.disconnect_dialog);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.setCancelable(false);
+        dialog.show();
+
+        AppCompatButton yesBtn = dialog.findViewById(R.id.yes_Btn);
+        AppCompatButton noBtn = dialog.findViewById(R.id.no_Btn);
+        TextView description = dialog.findViewById(R.id.description_Textview);
+
+        String message = "Do you want to disconnect with Dr. " + doctorName + "?";
+        description.setText(message);
+
+        if (context.getApplicationContext() != null){
+            UserDetails userDetails = new UserDetails(context.getApplicationContext());
+
+            String patientId = userDetails.getPatientId();
+
+
+            yesBtn.setOnClickListener(v->{
+                FirebaseFirestore.getInstance().collection("Users").document(patientId)
+                        .collection("myDoctor").document(userIdString)
+                        .delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(context.getApplicationContext(), "You are now disconnect to Dr. " + doctorName, Toast.LENGTH_LONG).show();
+                                    context.startActivity(new Intent(context.getApplicationContext(), Homepage1.class));
+                                    dialog.dismiss();
+                                }
+                                else{
+                                    Toast.makeText(context.getApplicationContext(), "Failed to disconnect", Toast.LENGTH_LONG).show();
+                                    dialog.dismiss();
+                                }
+                            }
+                        });
+
+                FirebaseFirestore.getInstance().collection("userDoctor").document(userIdString)
+                        .collection("myPatient").document(patientId)
+                        .delete()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Log.d("TAG", "Disconnect doctor to patient");
+                                    dialog.dismiss();
+                                }
+                                else{
+                                    Log.d("TAG", "Failed to disconnect doctor to patient");
+                                    dialog.dismiss();
+                                }
+                            }
+                        });
+
+            });
+
+            noBtn.setOnClickListener(v->{
+                dialog.dismiss();
+            });
+        }
 
 
 
@@ -99,6 +179,7 @@ public class ConnectedWithAdapter extends RecyclerView.Adapter<ConnectedWithAdap
         String userId;
         String profilePic;
         TextView dateAndTime;
+        ImageButton disconnectBtn;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -106,6 +187,7 @@ public class ConnectedWithAdapter extends RecyclerView.Adapter<ConnectedWithAdap
             name = itemView.findViewById(R.id.name_Textview);
             imageview = itemView.findViewById(R.id.profile_pic_image_view);
             dateAndTime = itemView.findViewById(R.id.dateAndTimeConnected_Textview);
+            disconnectBtn = itemView.findViewById(R.id.disconnect_Button);
         }
     }
 }
